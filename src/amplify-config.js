983f23549ba35fb3cdf3fb386f1ad060cf0a9dbd
raju200539 <118793081+raju200539@users.noreply.github.com>
2025-06-23 15:59:42 +0530

    Update amplify-config.js

--- src/amplify-config.js
+++ src/amplify-config.js
@@ -1,8 +1,8 @@
 const awsExports = {
   Auth: {
     region: "us-east-1",
-    userPoolId: "us-east-1_99ECc0rOh",       // ✅ your pool ID
-    userPoolWebClientId: "ubf2u02i393cn2n7p097a4b81", // ✅ your app client ID
+    userPoolId: "us-east-1_99ECc0rOh",
+    userPoolWebClientId: "ubf2u02i393cn2n7p097a4b81",
   }
 };
 
